34b571b2b7a2795befa9a12382e9efacc18fefc5
SheKing <oofricoo@gmail.com>
2020-08-25 18:44:06 +0200

    implement animation for mouse hovering over keyboard keys

--- js/script.js
+++ js/script.js
@@ -1,3 +1,20 @@
 console.log("It's alive!");
 
-// add random classes to change key color when hovered or key pressed
+const colors = ['ff0000', '158000', 'ddbc00', 'ab00c2', '0059ff'];
+const defColor = 'bbbbbb';
+let curColor = colors[0];
+
+$(".keyboard-keys>.btn").hover(function () {
+    curColor = colors[Math.floor(Math.random() * colors.length)];
+
+    $(this).animate({
+        borderColor: `#${curColor}`,
+        color: `#${curColor}`,
+    }, 50);
+
+}, function () {
+    $(this).animate({
+        borderColor: `#${defColor}`,
+        color: `#${defColor}`
+    }, 50);
+});
